4f2736afe568f50f3a2b6ce2c4f3d8bef148de95
ClaraFurini <claraafurinii@gmail.com>
2022-04-06 13:06:23 -0300

    adicionei meu primeiro site htmk php

---
+++ HTML_CSS/projeto 1/Aplicação.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Primeiro site HTML!</title>
+	</head>
+	<body> 
+		
+		<?php
+			for ( $i = 0 ; $i<10 ; $i++) {
+				print( "Linha número " . $i . "<br />");
+			}
+		?>
+	</body>
+</html>
